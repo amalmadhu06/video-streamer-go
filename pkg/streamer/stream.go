4bde999f1f2597c325ce497af47c85d01f53de2d
Amal <amalmadhu06@gmail.com>
2023-06-02 10:53:17 +0530

    feat(stream): use go routines and channels for reading and serving content

--- pkg/streamer/stream.go
+++ pkg/streamer/stream.go
@@ -5,44 +5,58 @@ import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func Stream(c *gin.Context) {
-
-	//fetch video id from path parameter
+	// Fetch video id and playlist name from path parameters
 	videoID := c.Param("video_id")
-
-	//return error if video id is not there
-	if videoID == "" {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "failed to read video id from request",
-			"error":   "invalid request",
-		})
-		return
-	}
-
-	// fetch playlist name from path parameter
 	playlist := c.Param("playlist")
 
-	// file path for locating playlists
-	playlistPath := fmt.Sprintf("pkg/storage/%s/%s", videoID, playlist)
+	// Create a channel to receive the playlist data
+	playlistDataChan := make(chan []byte)
+	errChan := make(chan error)
 
-	playlistData, err := ioutil.ReadFile(playlistPath)
+	go func() {
+		// Fetch the playlist data in a separate goroutine
+		playlistData, err := readPlaylistData(videoID, playlist)
+		if err != nil {
+			errChan <- err // Send the error through the error channel
+			return
+		}
+		playlistDataChan <- playlistData // Send the playlist data through the data channel
+	}()
 
-	// handle error that may error while reading file
-	if err != nil {
+	select { //select helps to perform non-blocking operation on channels. It waits until one case is ready to proceed, and if multiple cases are ready to proceed, it randomly selects one to execure.
+	case playlistData := <-playlistDataChan: // if data is available on the playlistDataChan channel
+		// Set the response headers
+		c.Header("Content-Type", "application/vnd.apple.mpegurl")
+		c.Header("Content-Disposition", "inline")
+
+		// Write the playlist data to the response body
+		c.Writer.Write(playlistData)
+	case err := <-errChan: // This case checks if there is an error available on the errChan channel
+		// Handle the error
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "failed to read file from server",
 			"error":   err.Error(),
 		})
-		return
+	case <-time.After(5 * time.Second): //This case uses the time.After function to create a channel that sends a value after a specified duration.
+		// Handle the timeout case
+		c.JSON(http.StatusGatewayTimeout, gin.H{
+			"message": "request timed out",
+		})
 	}
+}
 
-	// Set the response headers
-	c.Header("Content-Type", "application/vnd.apple.mpegurl")
-	c.Header("Content-Disposition", "inline")
+func readPlaylistData(videoID, playlist string) ([]byte, error) {
+	// Construct the playlist file path
+	playlistPath := fmt.Sprintf("pkg/storage/%s/%s", videoID, playlist)
 
-	// Write the playlist data to the response body
-	c.Writer.Write(playlistData)
-
+	// Read the playlist file
+	playlistData, err := ioutil.ReadFile(playlistPath)
+	if err != nil {
+		return nil, err
+	}
+	return playlistData, nil
 }
